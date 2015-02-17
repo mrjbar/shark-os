0783e460904fa11d07b6956ffc3a77d9b44edc10
mrjbar <jabari.lofton@sjsu.edu>
2015-02-16 21:56:01 -0800

    Added individual processes

--- main.java
+++ main.java
@@ -18,7 +18,7 @@ public class main {
      // Runnable r1 = new Process(burstTime, arrivalTime, priority, turnAroundTime, waitingTime, id, timeLeftInQuantum, expectedFinishTime, endTime);
         
         ArrayList<Process> processList = new ArrayList<>();        
-        for ( int i = 0 ; i < 10 ; i++) {
+        for ( int i = 0 ; i < 30 ; i++) {
            processList.add(new Process(i, expecRunTime(), ranArrival(), priority()));
         }
         
